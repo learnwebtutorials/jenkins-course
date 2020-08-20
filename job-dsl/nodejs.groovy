3ff0811ac35f46840c4b856311abd6f5ae576ccf
LearnWebTutorials <learnwebtutorials@users.noreply.github.com>
2020-08-19 20:34:54 -0700

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,15 +1,15 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/learnwebtutorials/hello-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Allen')
+            node / gitConfigEmail('info@blissfulwebdesign.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('Node JS') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
